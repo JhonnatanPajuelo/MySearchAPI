ed188f746fec00bfe2368c39f906cadc287c0584
jpajuelo <jpajuelogibaja@gmail.com>
2024-11-25 20:32:37 -0500

    Inicio del proyecto

---
+++ src/main/java/com/jpajuelo/mysearchapi/MySearchApiApplication.java
@@ -0,0 +1,13 @@
+package com.jpajuelo.mysearchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySearchApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySearchApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jpajuelo/mysearchapi/MySearchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpajuelo.mysearchapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySearchApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
